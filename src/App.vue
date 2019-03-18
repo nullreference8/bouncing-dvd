ea640142db0301ad150190402a2b5a9da3fc01d3
xsidekick409 <xsidekick409@gmail.com>
2019-03-17 20:13:44 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Dvd/>
+  </div>
+</template>
+
+<script>
+import Dvd from './components/Dvd.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Dvd
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #2c3e50;
+  min-height: 100vh;
+  width: 100%
+}
+</style>
